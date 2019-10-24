a11e0cc9562b00dc6b372730825ce783feb071e8
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-10-24 16:23:45 -0700

    bloop

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -1,6 +1,5 @@
 #!python
 
-
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
@@ -15,7 +14,6 @@ def is_sorted(items):
             return False
     return True
 
-
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
@@ -29,7 +27,26 @@ def bubble_sort(items):
             if items[i] > items[i+1]:
                 items[i], items[i+1] = items[i+1], items[i]
 
+    # """Here's what Nathan Pillai had:"""
 
+    # index_1 = 0
+    # index_2 = 1
+
+    # while is_sorted(items) == False:
+
+    #     if items[index_1] > items[index_2]:
+    #         temp = items[index_1]
+    #         items[index_1] = items[index_2]
+    #         items[index_2] = temp
+
+    #     index_1 += 1
+    #     index_2 += 1
+
+    # index_1 = 0
+    # index_2 = 1
+
+
+"""In process"""
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
@@ -39,6 +56,11 @@ def selection_sort(items):
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
 
+    while is_sorted(items) is False:
+        for i in range (0, len(items)-1):
+            current_min = items[0] #first unsorted element
+            if items[i] < current_min:
+                current_min, items[i] = items[i], current_min
 
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
---
+++ Code/z_sandbox.py
@@ -0,0 +1,8 @@
+array = [0, 1, 2, 3, 4, 5]
+print('OG array --> ' + str(array))
+print('-----')
+print('array[:2] --> ' + str(array[:2]))
+print('So, FIRST TWO indices pls')
+print('-----')
+print('array[2:] --> ' + str(array[2:]))
+print('So, everything BUT the first two indices pls')
